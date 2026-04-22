cc42e9e632bb0192788dc0fb5d2dfd713e7f1d33
Roel Smits <roelsmits@Roels-MacBook-Pro.local>
2026-04-22 12:43:00 -0500

    button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -79,7 +79,7 @@ export function Navbar({ language, onLanguageChange }: NavbarProps) {
             ))}
             <LanguageSelector currentLang={currentLang} onLanguageChange={onLanguageChange} />
             <Button
-              className="bg-gold text-black hover:bg-gold/90 text-sm px-4 py-2"
+              className="bg-gold text-black hover:bg-gold/90 text-sm px-5 py-2.5 rounded-full transition-all"
               onClick={() => handleNavClick("#contact")}
             >
               {t.nav.freeConsultation}
